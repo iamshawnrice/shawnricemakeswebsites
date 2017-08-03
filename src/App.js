f24dca2bd9b5e1e5b36593386f2974a2e066892f
Shawn Rice <iamshawnrice@gmail.com>
2017-08-02 22:53:10 -0400

    Work out isMobile logic/State Management

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { debounce } from 'lodash';
 
 import data from './data';
 import utils from './utils';
@@ -10,35 +11,51 @@ import Portfolio from './components/portfolio/portfolio';
 import PortfolioItem from './components/portfolio/portfolioitem';
 
 class App extends React.Component {
-  state = {
+	state = {
 		items: data.portfolio.items,
 		isMobile: utils.isMobile()
-  }
+	}
 
-  renderPortfolio() {
-    return <Portfolio items={this.state.items} />;
-  }
+	renderPortfolio() {
+		return <Portfolio items={this.state.items} />;
+	}
 
-  renderPortfolioItem(args) {
-    return <PortfolioItem item={this.state.items[args.match.params.id]} />;
-  }
+	renderPortfolioItem(args) {
+		return <PortfolioItem item={this.state.items[args.match.params.id]} />;
+	}
 
-  render() {
-    return (
-      <div>
-        <Router>
-          <div>
-            <Header ismobile={this.state.isMobile}/>
+	recalculateWindowWidth() {
+		function _setState() {
+			this.setState(prevState => Object.assign({}, prevState, {isMobile: utils.isMobile()}) );
+		}
 
-            <Route path="/portfolio/" exact render={this.renderPortfolio.bind(this)} />
-            <Route path="/portfolio/:id" render={this.renderPortfolioItem.bind(this)} />
+		debounce(_setState, 100);
+	}
 
-            <Footer ismobile={this.state.isMobile}/>
-          </div>
-        </Router>
-      </div>
-    );
-  }
+	componentDidMount() {
+		window.addEventListener('resize', this.recalculateWindowWidth.bind(this));
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.recalculateWindowWidth.bind(this));
+	}
+
+	render() {
+		return (
+			<div>
+				<Router>
+					<div>
+						<Header ismobile={this.state.isMobile}/>
+
+						<Route path="/portfolio/" exact render={this.renderPortfolio.bind(this)} />
+						<Route path="/portfolio/:id" render={this.renderPortfolioItem.bind(this)} />
+
+						<Footer ismobile={this.state.isMobile}/>
+					</div>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
