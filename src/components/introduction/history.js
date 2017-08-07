fa3656b259711ea98c5bb23541bfaaff90404ac1
Shawn Rice <iamshawnrice@gmail.com>
2017-08-06 21:02:02 -0400

    Begin working on the history component

--- src/components/introduction/history.js
+++ src/components/introduction/history.js
@@ -20,7 +20,14 @@ const HistorySection = styled.section`
 class History extends React.Component {
 	render() {
 		return (
-			<HistorySection />
+			<HistorySection>
+				<div>
+					<p>Some Time in 2011</p>
+					<h3>Junyo</h3>
+
+					<p>I was working as a rental agent for a real estate broker in Brooklyn, when my boss one day decided that I could make websites. Never one to back away from a challenge, I obtained a copy of Dreamweaver and a book on HTML. Within a week we had a perfectly horrible little 4 page site on our hands. Lucky for me, he was satisfied, paid me a few hundred dollars and I was never interested in showing another apartment again. This was what those in the world of  dramatic literature refer to as the ‘Inciting Incident’.</p>
+				</div>
+			</HistorySection>
 		);
 	}
 }
