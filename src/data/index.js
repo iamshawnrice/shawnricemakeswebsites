1843123883b9e049db21bc841707685ed2f4112b
Shawn Rice <iamshawnrice@gmail.com>
2017-08-10 23:59:15 -0400

    Add about data

--- src/data/index.js
+++ src/data/index.js
@@ -1,72 +1,77 @@
 const data = {
 	portfolio: {
-		"items" : {
-			"iamheatherrice" : {
-				"background" : "bg0",
-				"description" : "Heather Rice is a singer/songwriter who just released her first full-length album of original music. She is also my wife. She needed a site that was responsive, easy to maintain, and allowed her to build and connect with an audience all over the globe. Designed by the extremely talented <a href='https://dribbble.com/Jonesie'>Robert Jones.</a>",
-				"id" : "iamheatherrice",
-				"tags" : "wordpress sass bandsintown",
-				"title" : "iamheatherrice.com",
-				"url" : "http://iamheatherrice.com",
-				"featuredimage": "/images/portfolio/iamheatherrice/iamheatherrice-preview.jpg"
+		// http://ashleymorganbloom.com/
+		// https://hglightingdesign.com/
+		// http://thebrolodex.com/
+		// http://iamshawnrice.com
+		// Documentation
+		'items' : {
+			'iamheatherrice' : {
+				'background' : 'bg0',
+				'description' : 'Heather Rice is a singer/songwriter who just released her first full-length album of original music. She is also my wife. She needed a site that was responsive, easy to maintain, and allowed her to build and connect with an audience all over the globe. Designed by the extremely talented <a href="https://dribbble.com/Jonesie">Robert Jones.</a>',
+				'id' : 'iamheatherrice',
+				'tags' : 'wordpress sass bandsintown',
+				'title' : 'iamheatherrice.com',
+				'url' : 'http://iamheatherrice.com',
+				'featuredimage': '/images/portfolio/iamheatherrice/iamheatherrice-preview.jpg'
 			},
-			"sample-item-1" : {
-				"background" : "bg0",
-				"description" : "Cornhole fanny pack copper mug brunch. Try-hard keytar sartorial four dollar toast cronut crucifix trust fund shaman ugh. Sriracha readymade jianbing, ugh knausgaard taxidermy keytar tattooed four loko vegan ethical schlitz cardigan. Semiotics food truck you probably haven't heard of them locavore chambray banh mi tousled, meditation bicycle rights. Keffiyeh vape vexillologist jean shorts chartreuse. Hella synth four dollar toast, try-hard roof party brooklyn poutine pop-up meggings edison bulb. Plaid meditation austin slow-carb vegan mustache humblebrag celiac VHS hexagon. Waistcoat freegan kitsch, organic cronut food truck art party poutine jianbing hell of. Affogato authentic trust fund snackwave taiyaki cronut knausgaard ramps thundercats green juice quinoa aesthetic gochujang jianbing cardigan. Heirloom lumbersexual bicycle rights farm-to-table hoodie tousled waistcoat celiac pour-over chambray fingerstache sartorial live-edge microdosing readymade. 8-bit ugh pop-up bushwick, occupy vice austin hell of. Irony taxidermy marfa cred thundercats. Green juice shaman street art drinking vinegar coloring book. Church-key keytar etsy affogato, asymmetrical bicycle rights af gochujang raw denim PBR&B ramps.",
-				"id" : "sample-item-1",
-				"tags" : "wordpress angular sass",
-				"title" : "iamshawnrice.com",
-				"url" : "http://example.com",
-				"featuredimage": "/images/portfolio/sample-item-1/featured/featured.jpg"
+			'sample-item-1' : {
+				'background' : 'bg0',
+				'description' : 'Cornhole fanny pack copper mug brunch. Try-hard keytar sartorial four dollar toast cronut crucifix trust fund shaman ugh. Sriracha readymade jianbing, ugh knausgaard taxidermy keytar tattooed four loko vegan ethical schlitz cardigan. Semiotics food truck you probably haven’t heard of them locavore chambray banh mi tousled, meditation bicycle rights. Keffiyeh vape vexillologist jean shorts chartreuse. Hella synth four dollar toast, try-hard roof party brooklyn poutine pop-up meggings edison bulb. Plaid meditation austin slow-carb vegan mustache humblebrag celiac VHS hexagon. Waistcoat freegan kitsch, organic cronut food truck art party poutine jianbing hell of. Affogato authentic trust fund snackwave taiyaki cronut knausgaard ramps thundercats green juice quinoa aesthetic gochujang jianbing cardigan. Heirloom lumbersexual bicycle rights farm-to-table hoodie tousled waistcoat celiac pour-over chambray fingerstache sartorial live-edge microdosing readymade. 8-bit ugh pop-up bushwick, occupy vice austin hell of. Irony taxidermy marfa cred thundercats. Green juice shaman street art drinking vinegar coloring book. Church-key keytar etsy affogato, asymmetrical bicycle rights af gochujang raw denim PBR&B ramps.',
+				'id' : 'sample-item-1',
+				'tags' : 'wordpress angular sass',
+				'title' : 'iamshawnrice.com',
+				'url' : 'http://example.com',
+				'featuredimage': '/images/portfolio/sample-item-1/featured/featured.jpg'
 			},
-			"sample-item-2" : {
-				"background" : "bg1",
-				"id" : "sample-item-2",
-				"title" : "example2.com"
+			'sample-item-2' : {
+				'background' : 'bg1',
+				'id' : 'sample-item-2',
+				'title' : 'example2.com'
 			},
-			"sample-item-3" : {
-				"background" : "bg2",
-				"id" : "sample-item-3",
-				"title" : "example3.com"
+			'sample-item-3' : {
+				'background' : 'bg2',
+				'id' : 'sample-item-3',
+				'title' : 'example3.com'
 			},
-			"sample-item-4" : {
-				"background" : "bg3",
-				"id" : "sample-item-4",
-				"title" : "example4.com"
+			'sample-item-4' : {
+				'background' : 'bg3',
+				'id' : 'sample-item-4',
+				'title' : 'example4.com'
 			},
-			"sample-item-5" : {
-				"background" : "bg4",
-				"id" : "sample-item-5",
-				"title" : "example5.com"
+			'sample-item-5' : {
+				'background' : 'bg4',
+				'id' : 'sample-item-5',
+				'title' : 'example5.com'
 			},
-			"sample-item-6" : {
-				"background" : "bg5",
-				"id" : "sample-item-6",
-				"title" : "example6.com"
+			'sample-item-6' : {
+				'background' : 'bg5',
+				'id' : 'sample-item-6',
+				'title' : 'example6.com'
 			},
-			"sample-item-7" : {
-				"background" : "bg6",
-				"id" : "sample-item-7",
-				"title" : "example7.com"
+			'sample-item-7' : {
+				'background' : 'bg6',
+				'id' : 'sample-item-7',
+				'title' : 'example7.com'
 			}
 		}
 	},
 	pages: {
 		about: {
 			image: {
-				file: "about",
-				alt: "Shawn & Heather Rice"
+				file: 'about',
+				alt: 'Shawn & Heather Rice'
 			},
-			copy: "Cornhole fanny pack copper mug brunch. Try-hard keytar sartorial four dollar toast cronut crucifix trust fund shaman ugh. Sriracha readymade jianbing, ugh knausgaard taxidermy keytar tattooed four loko vegan ethical schlitz cardigan. Semiotics food truck you probably haven't heard of them locavore chambray banh mi tousled, meditation bicycle rights. Keffiyeh vape vexillologist jean shorts chartreuse. Hella synth four dollar toast, try-hard roof party brooklyn poutine pop-up meggings edison bulb. Plaid meditation austin slow-carb vegan mustache humblebrag celiac VHS hexagon. Waistcoat freegan kitsch, organic cronut food truck art party poutine jianbing hell of. Affogato authentic trust fund snackwave taiyaki cronut knausgaard ramps thundercats green juice quinoa aesthetic gochujang jianbing cardigan. Heirloom lumbersexual bicycle rights farm-to-table hoodie tousled waistcoat celiac pour-over chambray fingerstache sartorial live-edge microdosing readymade. 8-bit ugh pop-up bushwick, occupy vice austin hell of. Irony taxidermy marfa cred thundercats. Green juice shaman street art drinking vinegar coloring book. Church-key keytar etsy affogato, asymmetrical bicycle rights af gochujang raw denim PBR&B ramps."
+			copy: 'I live in Charleston, SC with my wife (she’s the cute one on the right). We are in charge of feeding and entertaining a black Lab named Red and an orange tabby named CC (though I pretty much universally refer to him as Cecil). We are in the process of fixing up a cute little house in West Ashley in preparation for the arrival of our first child. You can learn more about me <a href="http://iamshawnrice.com">here</a> and more about Heather <a href="http://iamheatherrice.com">here</a>.'
 		},
 		introduction: {
 			image: {
-				file: "headshot",
-				alt: "Shawn Rice"
+				file: 'headshot',
+				alt: 'Shawn Rice'
 			},
 			introcopy: {
-				heading: "Hi. I'm Shawn Rice.",
-				introduction: "I am a web developer living and working in Charleston, SC. I am a firm believer in the path of least resistance and maintain that the simplest solution is the best one. My career started in the arts world so I am well accustomed to communicating and collaborating with people from diverse disciplines and backgrounds. And it has given me a tremendous amount of empathy for non-technical users. However, the journey from under-employed actor/failing real-estate agent to mid-level web developer did not occur overnight. It all started when..."
+				heading: 'Hi. I’m Shawn Rice.',
+				introduction: 'I am a web developer living and working in Charleston, SC. I am a firm believer in the path of least resistance and maintain that the simplest solution is the best one. My career started in the arts world so I am well accustomed to communicating and collaborating with people from diverse disciplines and backgrounds. And it has given me a tremendous amount of empathy for non-technical users. However, the journey from under-employed actor/failing real-estate agent to mid-level web developer did not occur overnight. It all started when...'
 			},
 			milestones: [
 				{
