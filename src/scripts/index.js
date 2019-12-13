43964ac50cd466bd111db7eebc6de2e253c7d2e8
Shawn Rice <iamshawnrice@gmail.com>
2019-12-13 13:55:27 -0500

    Style tweaks & get ready for JS

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,17 @@
-console.log("working...");
+(() => {
+    function docReady(fn) {
+        // see if DOM is already available
+        if (document.readyState === "complete" || document.readyState === "interactive") {
+            // call on next available tick
+            setTimeout(fn, 1);
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
+        }
+    }
+
+    docReady(init);
+
+    function init() {
+        console.log("reaaaaady");
+    }
+})()
