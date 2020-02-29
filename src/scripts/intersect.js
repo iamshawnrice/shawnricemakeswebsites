675e0be0ec43f0723e5c107f53e0b8372792c841
Shawn Rice <iamshawnrice@gmail.com>
2020-02-29 13:59:43 -0500

    WIP - fine tune reveal animations

--- src/scripts/intersect.js
+++ src/scripts/intersect.js
@@ -10,15 +10,39 @@ export function initIntersectObservers(opts) {
 
     function callback(entries) {
         entries.forEach((entry) => {
-            const { intersectionRatio, isVisible, target } = entry;
+            const { intersectionRatio, target } = entry;
 
-            console.log(intersectionRatio);
-        })
+            if (target.dataset.intersectState === "ready" && intersectionRatio >= .5) {
+                prereveal.call(undefined, target);
+            }
+
+            if (target.dataset.intersectState === "steady" && intersectionRatio >= .75) {
+                reveal.call(undefined, target);
+            }
+        });
+    }
+
+    function prereveal(target) {
+        target.dataset.intersectState = "steady";
+
+        if ("bg" in target.dataset) {
+            target.setAttribute("style", `background-color: ${target.dataset.bg}`);
+        }
+    }
+
+    function reveal(target) {
+        target.dataset.intersectState = "go";
     }
 
     const targets = document.querySelectorAll(".js-intersect-item");
+
     targets.forEach((target) => {
-        observer.observe(target);
-        target.dataset.intersectState = "ready";
+        const milestoneEl = target;
+        observer.observe(milestoneEl);
+        milestoneEl.dataset.intersectState = "ready";
+
+        if (milestoneEl.querySelector(".js-intersect-title")) {
+            milestoneEl.querySelector(".js-intersect-title").setAttribute("style", "transform: translate(50%, 10px)");
+        }
     });
 }
